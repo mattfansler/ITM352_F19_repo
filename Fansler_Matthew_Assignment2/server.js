7bb6d4810c7e4b4b6a62c3fa7985fd08ecba2048
mattfansler <54603668+mattfansler@users.noreply.github.com>
2019-12-04 01:36:10 -1000

    Add files via upload

--- Fansler_Matthew_Assignment2/server.js
+++ Fansler_Matthew_Assignment2/server.js
@@ -7,15 +7,54 @@ var app = express(); // Create an object with express
 var fs = require('fs'); //require a file system from node
 var myParser = require("body-parser"); //needed to make form data to be available in request.body
 //var services = require('./public/service_data.js'); // location of services
+var products = require("./public/service_data.js"); 
 const service_data = require('./public/service_data.js'); //keep service_data constant
 var filename = "user_data.json"; //location of user reg data
 //var user_quantity_data; // hold quantity variables until invoice is displayed
+const querystring = require("./public/service_data.js");
 
-app.use(myParser.urlencoded({ extended: true})); //use myParser
+try {
+    fs.chmodSync('./user_data.json', '777');
+} catch (err) {
+    console.log("Could not set!");
+}
 
-app.all('*', function (request, response, next) {    //Initialize express
+var users = require('./user_data.json');
+
+app.use(myParser.urlencoded({ extended: true })); //Use of myParser (JSON)
+app.use(express.json()); //Parsing of data
+app.post("/process_form", function (request, response) { //Code from Lab 13
+    let POST = request.body; 
+    var hasValidQuantities = true; //defining the hasValidQuantities variable and assuming all quantities are valid
+    var hasPurchases = false; //assume the quantity of purchases are false
+    for (i = 0; i < products.length; i++) { //for loop for each product array that increases the count by 1
+        q = POST['quantity' + i]; //quantity entered by the user for a product is aessigned into q
+        if (isNonNegInt(q) == false) { //if the quantity enetered by the user is invalid integer
+            hasValidQuantities = false; //hasValidQuantities is false or nothing was inputed in the quantity textbox
+        }
+        if (q > 0) { //if quantity entered in the textbox is greater than 0
+            hasPurchases = true; //if q is greater than 0 than the hasPurchases is ok
+        }
+    }
+    //if data is valid give user an invoice, if not give them an error
+    qString = querystring.stringify(POST); //string query together
+    if (hasValidQuantities == true && hasPurchases == true) { //if both hasValidQuantities variable and hasPurchases variable are valid 
+        response.redirect('./login_page.html?' + qString); //if quantity is valid it will send user to invoice
+    }
+    else {
+        response.redirect("./products_display.html?" + qString); //if quantity is invalid it will send user back to products page
+    }
+});
+
+
+app.all('*', function (request, response, next) {    //Initialize express, log requests and send back
     console.log(`${request.method} + ' to' + ${request.path}`);
     next();
+});
+
+// Endpoint to get all products
+app.get("/api/services", function (req, res) {
+    res.json(inventory);
 });
 
 // Only open the file if it exists Lab 14
@@ -34,7 +73,12 @@ if (fs.existsSync(filename))
  
 app.post("/login.html", function (request, response) { //Lab14
     // Process login form POST and redirect to logged in page if ok, back to login page if not
+    console.log(request.body); //To view terminal
+    var qString = querystring.stringify(request.query); //Put query together
     var errors = []; //create a blank errors variable
+    inputUser = request.body.username;
+    inputPass = request.body.password;
+    the_username = request.body.username.toLowerCase(); //Assigns the username from object
     newUsername = request.body.username.toLowerCase();  //convert to all lowercase username to prevent case errors in the future
     newPassword = request.body.password;
     if (typeof users_reg_data[newUsername] != 'undefined') {  //check if username already exists in user reg data
@@ -185,7 +229,7 @@ app.get('/purchase', function (request, response, next) { //get data from /purch
         }
         console.log(validQuantities, validPurchases); //log into console to check validity
     }
-    qString = qs.stringify(grab); //string query together
+    qString = qs.stringify(grab); //string query together, redirect user to login with query
     if (validQuantities == true && validPurchases == true) { //if both are true
         response.redirect('./login.html?' + qs.stringify(request.query)); //send to checkout with the form data 
     } else { //if either is false
@@ -262,11 +306,79 @@ function isNonNegInt(q, sendArrayBack = false) {  //Function from lab 11
     }
  });
 */
- app.post("/register", function (request, response) {
+ app.post('/processRegister', function (request, response) { //Request to register a new user
     // process a simple register form
+    let message = {};
+    let messages = []; //array for holding messages
+    let userData = req.body;
+    let userExists = typeof users[req.body.username] === 'undefined' ? false : true; //find the user
+    if (userExists) {
+        messages.push("Username is already taken")
+    } //Check if user does not exist
+    if (userData.username.length <6 || userData.username.length > 12) { //To make sure only numbers and letters are valid in username (6-12)
+        messages.push("Username must be between 6 to 12 characters!")
+    }
+    if (userData.password < 5) { //To make sure a user's password has the minimum count
+        messages.push("Password must a minimum of 5 characters!")
+    }
+    if (userData.password !== userData.passwordConfirm) { //Check if both passwords by new user is a match
+        messages.push("Passwords do not match!")
+    }
+    if (messages.length === 0) { //Add user if registration is done
+        users[userData.username] = { //Combine to object
+            name: userData.name,
+            password: userData.password,
+            email: userData.email
+        };
+
+        try {
+            fs.chmodSync('./users.json', '777');
+        } catch (err) {
+            messages.push("chown error: " + err + '\n');
+            
+        }
+
+        try {
+            fs.writeFileSync('./users.json', JSON.stringify(users));
+
+            messages.push('Account created');
+            // File is written sucessfully 
+            message = {
+                success: true,
+                msg: messages
+            }
+        } catch (err) {
+            messages.push("Something went terribly wrong creating your account\n" + err);
+
+            // Do not write to file
+            message = {
+                success: false,
+                msg: messages
+            }
+
+            // Remove from internal user objects
+            delete users[userData.username];
+        }
+    } else {
+        message = {
+            success: false,
+            msg: messages
+        }
+    }
+
+    // Send message back to request
+    if (message.success) {
+        res.status(200).json(message);
+    } else {
+        console.log(message);
+        res.status(400).json(message);
+    }
+});
+
     console.log("Got the registration request");
     let POST = request.body; //private variable that only effects this portion of the page
     username = POST.username;  //name that is specified in app.get register name="username"
+    userData.username = userData.username.toLowerCase().trim(); //allows user to use lower case in username
 if (typeof users_reg_data[username] == 'undefined') {
     users_reg_data[username] = {};  //create empty object
     users_reg_data[username].name = username; //
@@ -284,9 +396,8 @@ if (typeof users_reg_data[username] == 'undefined') {
     response.send("user " + username + " registered");
 } else
 {
-    response.send("User " + username + " already taken; try again.:" );
+response.send("User " + username + " already taken; try again.:" );
 }
- });
 
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
